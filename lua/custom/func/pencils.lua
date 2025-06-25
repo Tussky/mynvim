361843c50d7c39e92a8177bdc972b96eb43a5a21
tusky <isaac@dw-perspective.org.uk>
2025-06-25 08:28:00 -0400

    (tweak): Added function Landscape() - changed nerd_font: true

--- lua/custom/func/pencils.lua
+++ lua/custom/func/pencils.lua
@@ -4,4 +4,9 @@ function ColorMyPencils()
   vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
 end
 
+function Landscape()
+  vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+  vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+end
+
 ColorMyPencils()
