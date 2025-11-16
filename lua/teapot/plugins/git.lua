25469455ca3b9005bccce02c2c301ce077774d03
tusky <isaac@dw-perspective.org.uk>
2025-11-16 11:40:59 -0500

    Added fugitive and signs

---
+++ lua/teapot/plugins/git.lua
@@ -0,0 +1,14 @@
+return {
+	-- vim fugitive
+	{ "tpope/vim-fugitive", },
+	-- git signs
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			signcolumn = true, 
+			numhl = true,
+			linehl = true,
+		},
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+}
