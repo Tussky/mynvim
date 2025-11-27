898d4fea6a931d55a161b614f24c17fe77b4540d
tusky <isaac@dw-perspective.org.uk>
2025-11-27 12:05:25 -0500

    Feature(Adding lsp warning tags)

--- init.lua
+++ init.lua
@@ -11,6 +11,32 @@ vim.opt.cursorline = true
 vim.cmd("colorscheme gruvbox")
 
 
+-- start a lsp client and attach client to buffer
+vim.lsp.start_client({
+	name = "my-server",
+	cmd = {'lua-language-server'},
+	root_dir = vim.fs.dirname(vim.fs.find({'.git', 'pyproject.toml', 'setup.py'}, {upward = true})[1])
+})
+-- configure how the lsp client is displaying your text
+vim.diagnostic.config({
+	virtual_text = true,
+	signs = true, 
+	underline = true,
+	update_in_insert = true,
+})
+
+-- toggle how the lsp is displaying text
+toggle = true -- start by defining the toggle
+function toggle_lsp()
+	vim.diagnostic.config({
+		virtual_text = toggle,
+		signs = toggle, 
+		underline = toggle,
+		update_in_insert = toggle,
+	})
+	toggle = not toggle
+end
+toggle_lsp()
 -- my aliases
 vim.api.nvim_create_user_command('Ox', 'Oil --float', {desc = "Open file tree with oil in floating window."})
 
